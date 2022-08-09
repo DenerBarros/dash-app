1b9308bbb8ed7a0ae2259886fe0100b9420ebe40
DenerBarros <denerbarros.eng@gmail.com>
2022-08-09 09:47:24 -0300

    Criado o componente de formulário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 
 const Home: NextPage = () => {
@@ -21,37 +22,8 @@ const Home: NextPage = () => {
             flexDir='column'
           >
           <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              name='email'
-              id='email'
-              type='email'
-              focusBorderColor='pink.500'
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-              
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="email">Password</FormLabel>
-            <Input
-              name='password'
-              type='password' 
-              id='password'             
-              focusBorderColor='pink.500'
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>  
+            <Input name='email' type='email' label='E-mail' />
+            <Input name='password' type='password' label='Senha' />      
           </Stack>
 
             <Button type="submit" mt="6" colorScheme='pink' size="lg">Entrar</Button>
